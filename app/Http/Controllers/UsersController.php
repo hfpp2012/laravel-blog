f45180358a31ec57aae43607f1357d546ad87a6f
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2017-09-06 17:23:11 +0800

    seed & foreach

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,17 +9,7 @@ class UsersController extends Controller
 {
     public function index()
     {
-        $users = [
-            '0' => [
-                'first_name' => 'Renato',
-                'last_name' => 'Hysa'
-            ],
-            '1' => [
-                'first_name' => 'Jessica',
-                'last_name' => 'Alba'
-            ]
-        ];
-        // return $users;
+        $users = User::all();
         return view('admin.users.index', compact('users'));
     }
 
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,5 +1,20 @@
-<ul>
-    @foreach($users as $user)
-        <li>{{ $user['first_name'] }} {{ $user['last_name'] }}</li>
-    @endforeach
-</ul>
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <ul class='list-group'>
+                @foreach($users as $user)
+                    <li class='list-group-item' style="margin-top: 20px;">
+                        <span>{{ $user->name }}</span>
+                        <span class="pull-right clearfix">
+                            Joined ({{ $user->created_at->diffForHumans() }})
+                        </span>
+                        <button class='btn btn-xs btn-primary'>Follow</button>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+@endsection
+
